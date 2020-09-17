0311819ff5f97c69247af9255aca941529f7f2e5
John Jackal <45736444+JohnJackal@users.noreply.github.com>
2020-09-17 21:19:24 +0800

    Add files via upload

---
+++ MDSGAFIN.py
@@ -0,0 +1,9 @@
+import DenseBlock
+import torch
+import torch.nn as nn
+
+class MDSGAFIN(nn.Module):
+    def __init__(self,
+                 channel = 3,
+                 filter_size = 3):
+        pass
