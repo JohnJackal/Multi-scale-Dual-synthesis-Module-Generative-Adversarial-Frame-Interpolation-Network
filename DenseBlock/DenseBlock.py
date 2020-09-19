fe8e67be0fba309e644586d7038a8bc89f2ed8b2
John Jackal <45736444+JohnJackal@users.noreply.github.com>
2020-09-19 16:48:43 +0800

    update the DenseBlock module

--- DenseBlock/DenseBlock.py
+++ DenseBlock/DenseBlock.py
@@ -8,23 +8,29 @@ from torch.hub import load_state_dict_from_url
 from torch import Tensor
 from torch.jit.annotations import List
 
+
 # define the Mish activation function
 class Mish(nn.Module):
+
     def __init__(self):
         super(Mish, self).__init__()
-    def forward(self, input:Tensor) -> Tensor:
+
+    def forward(self, input: Tensor) -> Tensor:
         return mish(input)
 
-def mish(x:Tensor)-> Tensor:
+
+def mish(x: Tensor) -> Tensor:
     feature = x * (torch.tanh(F.softplus(x)))
     return feature
 
+
 class DenseLayer(nn.Module):    # module isn't a ordered structure.
-    def __init__(self,num_input_features,
-                 growth_rate,kernel_size,
-                 bn_size,drop_rate,
+
+    def __init__(self, num_input_features,
+                 growth_rate, kernel_size,
+                 bn_size, drop_rate,
                  memory_efficient=False):
-        super(DenseLayer,self).__init__()
+        super(DenseLayer, self).__init__()
         self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
         self.add_module('mish1', Mish())
         self.add_module('conv1',nn.Conv2d(num_input_features,growth_rate * bn_size,
@@ -36,20 +42,20 @@ class DenseLayer(nn.Module):    # module isn't a ordered structure.
         self.drop_rate = float(drop_rate)
         self.memory_efficient = memory_efficient
 
-    def bn_function(self,input:List(Tensor)) -> Tensor:
-        '''
+    def bn_function(self, input: List[Tensor]) -> Tensor:
+        """
             bottleneck layer function
             type: (List[Tensor]) -> Tensor
-        '''
+        """
         concated_features = torch.cat(input,1)
         bottleneck_output = self.conv1(self.mish1(self.norm1(concated_features)))
         return bottleneck_output
 
     def any_requires_grad(self,input:List[Tensor]) -> bool:
-        '''
+        """
             to detect whether there is any tensor requires grad calculation.
             returns boolean
-        '''
+        """
         for tensor in input:
             if tensor.requires_grad:
                 return True
@@ -57,31 +63,31 @@ class DenseLayer(nn.Module):    # module isn't a ordered structure.
 
     @torch.jit.unused
     def call_checkpoint_bottleneck(self,input:List[Tensor]) -> Tensor:
-        '''
+        """
             type: (List[Tensor]) -> Tensor
             perform checkpointed bottleneck.
-        '''
+        """
         def closure(*inputs):
             return self.bn_function(inputs)
 
         return cp.checkpoint(closure, *input)
 
     @torch.jit._overload_method  # noqa: F811
-    def forward(self, input):
+    def forward(self, inputs):
         # type: (List[Tensor]) -> (Tensor)
         pass
 
     @torch.jit._overload_method  # noqa: F811
-    def forward(self, input):
+    def forward(self, inputs):
         # type: (Tensor) -> (Tensor)
         pass
 
-    def forward(self,input):
+    def forward(self, inputs):
         # wrap the input to a List[Tensor]
-        if isinstance(input, Tensor):
-            prev_features = [input]
+        if isinstance(inputs, Tensor):
+            prev_features = [inputs]
         else:
-            prev_features = input
+            prev_features = inputs
 
         # if memory isn't enough then do the checkpointed bn, else do the bn directly.
         if self.memory_efficient and self.any_requires_grad(prev_features):
@@ -101,36 +107,41 @@ class DenseLayer(nn.Module):    # module isn't a ordered structure.
                                      training=self.training)
         return new_features
 
+
 # define the dense block
 class DenseBlock(nn.ModuleDict):    # moduledict is a dict of submodules. (submodules could be called via their names)
-    def __init__(self,num_layers,
+    _version = 2
+
+    def __init__(self, num_layers,
                  num_input_features,
-                 bn_size,growth_rate,
+                 bn_size, growth_rate,
                  drop_rate,
                  memory_efficient=False):
-        super(DenseBlock,self).__init__()
+        super(DenseBlock, self).__init__()
         for i in range(num_layers):
             layer = DenseLayer(
                 num_input_features + i * growth_rate,    # densenet's featuremaps would grow with layer number n increases.
-                growth_rate = growth_rate,
-                bn_size = bn_size,
-                drop_rate = drop_rate,
-                memory_efficient = memory_efficient,
+                growth_rate=growth_rate,
+                bn_size=bn_size,
+                drop_rate=drop_rate,
+                memory_efficient=memory_efficient,
             )
             self.add_module('denselayer%d' % (i+1), layer)
 
     def forward(self, init_features):
         features = [init_features]
+        # accumulate the features calculated by former layers and iterate on.
         for name, layer in self.items():
             new_features = layer(features)
             features.append(new_features)
-        return torch.cat(features,1)
+        return torch.cat(features, 1)
+
 
 # def transition layer class
 class TransitionLayer(nn.Sequential):   # Sequential is a ordered structure.
-    def __init__(self,num_input_features, num_output_features):
-        super(TransitionLayer,self).__init__()
+    def __init__(self, num_input_features, num_output_features):
+        super(TransitionLayer, self).__init__()
         self.add_module('norm', nn.BatchNorm2d(num_input_features))
         self.add_module('mish', Mish())
-        self.add_module('conv', nn.Conv2d(num_input_features,num_output_features,
-                                          kernel_size=1,stride=1,bias=False))
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+                                          kernel_size=1, stride=1, bias=False))
